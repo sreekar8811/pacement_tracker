ea273cb8d209fdacbb339a7fbae77cbd4413614e
sreekar8811 <141726175+sreekar8811@users.noreply.github.com>
2025-03-06 18:13:53 +0530

    wsgi.py

--- backend/users/wsgi.py
+++ backend/users/wsgi.py
@@ -1,6 +1,6 @@
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'your_project_name.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend.settings')
 
 application = get_wsgi_application()
